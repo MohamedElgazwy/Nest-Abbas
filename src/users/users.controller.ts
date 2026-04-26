6ebfe614777c531aa3fb6c6ec6f5c385ad380f1e
Mohamed Elgazwy <engmohamedaljazwi@gmail.com>
2026-04-26 22:58:58 +0200

    guards cont..

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Headers } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Headers, UseGuards } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { RegisterDto } from "./dtos/register.dto";
 import { LoginDto } from "./dtos/login.dto";
+import { AuthGuard } from "./guards/auth.guard";
 
 @Controller('api/users')
 export class UsersController {
@@ -19,6 +20,7 @@ export class UsersController {
         return this.usersService.login(body);
     }
 
+    @UseGuards(AuthGuard)
     @Get('/current-user')
     public getCurrentUser(@Headers() headers: any) {
         return this.usersService.getCurrentUser(headers.authorization);
